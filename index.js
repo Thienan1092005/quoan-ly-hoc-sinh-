c7c0ff5c5060a1e63d237d365c8c3c6a28a31272
Thienan1092005 <ogyminecraft497@gmail.com>
2024-04-28 15:19:52 +0700

    hoan thanh CRUD bang js thuan

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const getElements = (name) => {
   return document.querySelector(name);
 };
+let selected = null;
+const studentName = getElements(".name");
+const className = getElements(".className");
+const diemToan = getElements(".diemToan");
+const diemLy = getElements(".diemLy");
+const diemHoa = getElements(".diemHoa");
+const diemTB = getElements(".diemTrungBinh");
+const btnAdd = getElements(".btn");
+const baseAPI = "https://662b63d6de35f91de1581587.mockapi.io/StudentList";
+const tbody = document.querySelector(".tbody");
+const btnUpdate = getElements(".btnUpdate");
+let studentList = [];
 const callAPI = () => {
   const data = axios({
     method: "GET",
@@ -14,7 +26,7 @@ const callAPI = () => {
 const render = () => {
   let listTr = "";
   studentList.map((student) => {
-    listTr += `<tr><td>${student.id}</td><td>${student.name}</td><td>${student.class}</td><td>${student.diemToan}</td><td>${student.diemLy}</td><td>${student.diemHoa}</td><td>${student.diemTrungBinh}</td><td><button onclick = handleDelete(${student.id}${student.name}) class="delete">Delete</button><button class="edit">Edit</button></td></tr>`;
+    listTr += `<tr><td>${student.id}</td><td>${student.name}</td><td>${student.class}</td><td>${student.diemToan}</td><td>${student.diemLy}</td><td>${student.diemHoa}</td><td>${student.diemTrungBinh}</td><td><button onclick =handleDelete(${student.id}) class="delete">Delete</button><button onclick =handleEdit(${student.id}) class="edit">Edit</button></td></tr>`;
   });
   tbody.innerHTML = listTr;
 };
@@ -33,7 +45,6 @@ const addStudent = () => {
   });
   add.then(() => {
     callAPI();
-    render();
     studentName.value = "";
     className.value = "";
     diemTB.value = "";
@@ -42,9 +53,9 @@ const addStudent = () => {
     diemTB.value = "";
   });
 };
-const handleDelete = (id, name) => {
-  if (name == "Lý Vân Tư") {
-    return;
+const handleDelete = (id) => {
+  if (id == 1) {
+    alert("khong the xoa doi tuong dac biet !");
   } else {
     const deleteData = axios({
       method: "DELETE",
@@ -52,20 +63,43 @@ const handleDelete = (id, name) => {
     });
     deleteData.then(() => {
       callAPI();
-      render();
     });
   }
 };
-const studentName = getElements(".name");
-const className = getElements(".className");
-const diemToan = getElements(".diemToan");
-const diemLy = getElements(".diemLy");
-const diemHoa = getElements(".diemHoa");
-const diemTB = getElements(".diemTrungBinh");
-const btnAdd = getElements(".btn");
-const baseAPI = "https://662b63d6de35f91de1581587.mockapi.io/StudentList";
-const tbody = document.querySelector(".tbody");
-let studentList = [];
+const handleEdit = (id) => {
+  selected = studentList.find((student) => student.id == id);
+  const get = axios({
+    method: "GET",
+    url: `${baseAPI}/${id}`,
+  });
+  get.then((res) => {
+    let info = res.data;
+    studentName.value = info.name;
+    className.value = info.class;
+    diemToan.value = info.diemToan;
+    diemLy.value = info.diemLy;
+    diemHoa.value = info.diemHoa;
+    diemTB.value = info.diemTrungBinh;
+  });
+};
+const update = () => {
+  console.log(`selected is : ` + selected);
+  const updateData = axios({
+    method: "PUT",
+    url: `${baseAPI}/${selected.id}`,
+    data: {
+      name: studentName.value,
+      class: className.value,
+      diemToan: diemToan.value,
+      diemLy: diemLy.value,
+      diemHoa: diemHoa.value,
+      diemTrungBinh: diemTB.value,
+    },
+  });
+  updateData.then(() => {
+    callAPI();
+  });
+};
+btnUpdate.addEventListener("click", update);
 btnAdd.addEventListener("click", addStudent);
 callAPI();
-render();
